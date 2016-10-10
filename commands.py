932af2a531bdffa230b5e08897a0e97007dd9b1f
mpurdon <matthew@codenaked.org>
2016-10-10 03:08:51 -0400

    Added more functionality.

--- commands.py
+++ commands.py
@@ -12,6 +12,18 @@ class Command(object):
     client = None
 
     def __init__(self, client, users, channels, ims):
+        """
+        Initialize the command
+
+        Args:
+            client:
+            users:
+            channels:
+            ims:
+        """
+        self.settings = {
+            'check_logs': None
+        }
         self.client = client
         self.users = users
         self.channels = channels
@@ -19,11 +31,14 @@ class Command(object):
 
     def run(self, channel, user, value):
         """
-        Perform the actions of this command
 
-        :param channel:
-        :param user:
-        :param value:
+        Args:
+            channel:
+            user:
+            value:
+
+        Returns:
+
         """
         raise NotImplementedError('You must define a run method')
 
@@ -37,9 +52,13 @@ class HelloCommand(Command):
         """
         Perform the action of this command
 
-        :param channel:
-        :param user:
-        :param value:
+        Args:
+            channel:
+            user:
+            value:
+
+        Returns:
+
         """
         response = 'Hello, {}!'.format(self.users[user])
         self.client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
@@ -73,6 +92,85 @@ class BizzfuzzCommand(Command):
             else:
                 response = number
         except ValueError:
-            response = 'Please privide a number, "{}" is not valid.'.format(value)
+            response = '{}, please provide a number, "{}" is not valid.'.format(self.users[user], value)
         finally:
             self.client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
+
+
+class SetCommand(Command):
+    """
+    Update a command setting
+
+    """
+    def run(self, channel, user, value):
+        """
+
+        Args:
+            channel:
+            user:
+            value:
+
+        Returns:
+
+        """
+        setting, _, setting_value = value.partition(' ')
+        self.settings[setting] = setting_value
+        response = 'Okay {}, setting {} to {}'.format(self.users[user], setting, setting_value)
+        self.client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
+
+
+class JoinCommand(Command):
+    """
+    Tell the bot to join a channel
+
+    """
+    def run(self, channel, user, value):
+        """
+
+        Args:
+            channel:
+            user:
+            value:
+
+        Returns:
+
+        """
+        api_response = self.client.api_call('channels.join', channel=value)
+        if not api_response.get('ok'):
+            response = 'Sorry, {}, I could not join {} due to {}.'.format(self.users[user], value, api_response.get('error', 'an error'))
+            return self.client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
+
+        if api_response.get('already_in_channel', False):
+            response = 'But {}, I am already in {}'.format(self.users[user], value)
+            return self.client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
+
+        new_channel_info = api_response['channel']
+        new_channel_id = new_channel_info['id']
+        response = 'Okay {}, I am here now.'.format(self.users[user])
+        self.client.api_call('chat.postMessage', channel=new_channel_id, text=response, as_user=True)
+
+
+class LeaveCommand(Command):
+    """
+    Tell the bot to join a channel
+
+    """
+    def run(self, channel, user, value):
+        """
+
+        Args:
+            channel:
+            user:
+            value:
+
+        Returns:
+
+        """
+        response = 'Okay {}, I am leaving {}.'.format(self.users[user], self.channels[channel])
+        self.client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
+
+        api_response = self.client.api_call('channels.leave', channel=channel)
+        if not api_response.egt('ok', False):
+            response = 'Sorry, {}, I could not leave {} due to {}.'.format(self.users[user], self.channels[channel], api_response.get('error', 'an error'))
+            return self.client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
+
--- mattbot.py
+++ mattbot.py
@@ -3,7 +3,6 @@
 Implementation of the mattbot
 
 """
-
 import commands
 import os
 import requests
@@ -11,20 +10,17 @@ import sys
 import time
 
 from slackclient import SlackClient
+from tempfile import NamedTemporaryFile
 
-MATTBOT_ID = os.environ.get('MATTBOT_ID')
-MATTBOT_TOKEN = os.environ.get('MATTBOT_TOKEN')
+MATTBOT_NAME = 'mattbot'
+MATTBOT_TOKEN = None
 
-AT_MATTBOT = '<@{}>'.format(MATTBOT_ID)
-READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
+READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from fire hose
 
 slack_client = None
 slack_users = None
 slack_channels = None
 slack_ims = None
-
-
-
 
 
 def handle_command(channel, user, bot_command):
@@ -38,39 +34,129 @@ def handle_command(channel, user, bot_command):
 
     command_type, _, parameters = bot_command.partition(' ')
     command_class = '{}Command'.format(command_type.capitalize())
-    command_module = sys.modules[commands]
 
     try:
-        handler_class = getattr(command_module, command_class)
-        handler = handler_class(slack_client)
+        handler_class = getattr(commands, command_class)
+        handler = handler_class(slack_client, slack_users, slack_channels, slack_ims)
         return handler.run(channel, user, parameters)
     except AttributeError:
         response = 'Not sure what you mean.'
         slack_client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
 
 
-def handle_deploy_log(file):
+def check_deploy_errors(lines):
+    """
+
+    Args:
+        lines:
+
+    Returns:
+
+    """
+    for line in lines:
+        if 'Error' in line or 'Traceback' in line:
+            return True
+
+    return False
+
+
+def download_file(url):
+    """
+
+    Args:
+        url:
+    """
+    print('Getting file contents from "{}"'.format(url))
+
+    session = requests.Session()
+    session.headers['Authorization'] = 'Bearer {}'.format(MATTBOT_TOKEN)
+    response = session.get(url, stream=True)
+
+    if response.status_code != 200:
+        print('Could not download file from {}'.format(url))
+        return None
+
+    with NamedTemporaryFile(delete=False) as log_file:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                log_file.write(chunk)
+
+        print('Saved log to {file.name}'.format(file=log_file))
+        return log_file.name
+
+
+def handle_deploy_log(channel, file):
     """
     Handle a log file
 
     """
     start_log = '#### RUNNING MIGRATIONS'
-    end_log = ''
+    end_log = '#### DONE'
 
-    recipients = ['mpurdon']
+    print(file)
+    url = file.get('url_private_download')
+    # url = file.get('permalink_public')
 
-    url = file.get('url_private ')
-    file_contents = requests.get(url)
+    file_name = download_file(url)
+
+    if file_name is None:
+        return None
 
     found_migrations = False
     log_content = []
 
-    for line in file_contents:
-        if not found_migrations:
-            if start_log in line:
-                found_migrations = True
-            else:
-                continue
+    with open(file_name) as log:
+        for line in log:
+            if not found_migrations:
+                if start_log in line:
+                    print('Found the start of the log')
+                    found_migrations = True
+                else:
+                    continue
+
+            log_content.append(line)
+
+            if found_migrations and end_log in line:
+                print('Found the end of the log')
+                break
+
+    # Nuke the log file
+    os.unlink(file_name)
+
+    has_errors = check_deploy_errors(log_content)
+
+    # recipients = ['robosung', 'mpurdon']
+    recipients = ['mpurdon', ]
+    recipient_ids = [recipient_id for recipient_id, name in slack_users.items() if name in recipients]
+
+    if has_errors:
+        message = 'I found an issue in the {} file, check the snippet.'.format(file['name'])
+    else:
+        message = 'I could not find any issues in the {} file, check the snippet.'.format(file['name'])
+
+    snippet_file_name = 'snippet.{}'.format(file['name'])
+
+    user_ims = {v: k for k, v in slack_ims.items() if v in recipient_ids}
+
+    content = ''.join(log_content)
+
+    for recipient_id in recipient_ids:
+        im_id = user_ims[recipient_id]
+
+        print('Sending message to {} IM.'.format(slack_users[recipient_id]))
+        slack_client.api_call('chat.postMessage', channel=recipient_id, text=message, as_user=True)
+
+        print('Sending snippet {} to {}'.format(snippet_file_name, slack_users[recipient_id]))
+        api_response = slack_client.api_call('files.upload',
+                                             channel=im_id,
+                                             content=content,
+                                             filename=snippet_file_name,
+                                             as_user=True)
+
+        if not api_response.get('ok', False):
+            message = 'Sorry {}, I was not able to send the snippet to you due to {}.'.format(slack_users[recipient_id],
+                                                                                              api_response.get('error', 'an error'))
+            slack_client.api_call('chat.postMessage', channel=recipient_id, text=message, as_user=True)
 
 
 def handle_file(channel, user, file_data):
@@ -78,12 +164,15 @@ def handle_file(channel, user, file_data):
     Handle a file upload
 
     """
-    print('>> Handling file {} from channel {}'.format(file_data['name'], channel))
+    file_name = file_data['name']
+    print('>> Handling file {} from channel {} by {}'.format(file_name, channel, user))
 
-    if channel == 'foo':
-        return handle_deploy_log(file_data)
+    # if channel == 'foo':
+    if file_name.endswith('deploy.log'):
+        return handle_deploy_log(channel, file_data)
 
-    # slack_client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
+    response = 'I have nothing to say about that file.'
+    slack_client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
 
 
 def parse_slack_output(slack_rtm_output):
@@ -114,7 +203,7 @@ def parse_slack_output(slack_rtm_output):
             if channel in slack_ims:
                 channel_name = 'private messages'
 
-        if 'file' in output and channel_name == 'mpurdon':
+        if 'file' in output:
             return handle_file(channel_name, user, output['file'])
 
         text = output.get('text', '')
@@ -126,9 +215,10 @@ def parse_slack_output(slack_rtm_output):
         if channel in slack_ims and user != MATTBOT_ID:
             return handle_command(channel, user, text)
 
-        print('{user} says: "{message}" in {channel}'.format(user=slack_users[user],
-                                                             message=text,
-                                                             channel=channel_name))
+        if user != MATTBOT_ID:
+            print('{user} says: "{message}" in {channel}'.format(user=slack_users[user],
+                                                                 message=text,
+                                                                 channel=channel_name))
 
 
 def get_users():
@@ -188,21 +278,49 @@ def get_ims():
     return slack_ims
 
 
+def get_user_id(users, name):
+    """
+    Get the user id for th given username
+
+    Args:
+        users: dict
+        name: string
+
+    Returns:
+        integer
+
+    """
+    for user_id, user_name in users.items():
+        if user_name == name:
+            return user_id
+
+    return None
+
+
 if __name__ == "__main__":
 
+    MATTBOT_TOKEN = os.environ.get('MATTBOT_TOKEN')
+    print('Attempting to connect with token: {}'.format(MATTBOT_TOKEN))
     slack_client = SlackClient(MATTBOT_TOKEN)
 
     if not slack_client.rtm_connect():
-        print("Connection failed. Invalid Slack token or bot ID?")
+        print("Connection failed, invalid Slack token.")
         sys.exit()
 
     print('MattBot connected and running!')
 
     slack_users = get_users()
     print ('\n'.join([k+':'+v for k, v in slack_users.items()]))
+    MATTBOT_ID = get_user_id(slack_users, MATTBOT_NAME)
+    print('Bot id: {}'.format(MATTBOT_ID))
+    AT_MATTBOT = '<@{}>'.format(MATTBOT_ID)
+
     slack_channels = get_channels()
+    print('\nChannels')
     print ('\n'.join([k + ':' + v for k, v in slack_channels.items()]))
+
     slack_ims = get_ims()
+    print ('\nIMs')
     print ('\n'.join([k + ':' + v for k, v in slack_ims.items()]))
 
     while True:
