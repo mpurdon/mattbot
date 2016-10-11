1f78689a9b9ac7bfd045d995de63dd5a4ff41b54
mpurdon <matthew@codenaked.org>
2016-10-10 23:54:08 -0400

    initial class-based bot refactoring

---
+++ config.py
@@ -0,0 +1,65 @@
+"""
+Configuration
+
+"""
+
+
+class Configuration(object):
+    """
+
+    """
+    BOT_NAME = 'mattbot'
+    BOT_ENV_TOKEN = 'MATTBOT_TOKEN'
+
+    READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from fire hose
+
+    FILTER_WORDS = {
+        'skank',
+        'wetback',
+        'bitch',
+        'cunt',
+        'dick',
+        'douchebag',
+        'dyke',
+        'fag',
+        'nigger',
+        'tranny',
+        'trannies',
+        'paki',
+        'pussy',
+        'retard',
+        'slut',
+        'titt',
+        'tits',
+        'wop',
+        'whore',
+        'chink',
+        'fatass',
+        'shemale',
+        'nigga',
+        'daygo',
+        'dego',
+        'dago',
+        'gook',
+        'kike',
+        'kraut',
+        'spic',
+        'twat',
+        'lesbo',
+        'homo',
+        'fatso',
+        'lardass',
+        'jap',
+        'biatch',
+        'tard',
+        'gimp',
+        'gyp',
+        'chinaman',
+        'chinamen',
+        'golliwog',
+        'crip',
+        'raghead',
+        'negro',
+        'hooker',
+        'hitler'
+    }
--- mattbot.py
+++ mattbot.py
@@ -4,6 +4,7 @@ Implementation of the mattbot
 
 """
 import commands
+import config
 import os
 import requests
 import sys
@@ -12,36 +13,164 @@ import time
 from slackclient import SlackClient
 from tempfile import NamedTemporaryFile
 
-MATTBOT_NAME = 'mattbot'
-MATTBOT_TOKEN = None
 
-READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from fire hose
-
-slack_client = None
-slack_users = None
-slack_channels = None
-slack_ims = None
-
-
-def handle_command(channel, user, bot_command):
+class Message(object):
     """
-    Receives commands directed at the bot and determines if they
-    are valid commands. If so, then acts on the commands. If not,
-    returns back what it needs for clarification.
+    A message in the RTM
 
     """
-    print('>> Handling command {} from channel {}'.format(bot_command, channel))
+    def __init__(self, bot, content):
+        """
+        Represents a message from the Slack RTM firehose
 
-    command_type, _, parameters = bot_command.partition(' ')
-    command_class = '{}Command'.format(command_type.capitalize())
+        """
+        message_type = content.get('type', None)
+        if message_type != 'message':
+            raise AttributeError('{} is not a valid message type.'.format(message_type))
 
-    try:
-        handler_class = getattr(commands, command_class)
-        handler = handler_class(slack_client, slack_users, slack_channels, slack_ims)
-        return handler.run(channel, user, parameters)
-    except AttributeError:
-        response = 'Not sure what you mean.'
-        slack_client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
+        user = content.get('user')
+
+        channel = content.get('channel')
+        try:
+            channel_name = slack_channels[channel]
+        except KeyError:
+            if channel in slack_ims:
+                channel_name = 'private messages'
+
+        if 'file' in content:
+            return handle_file(channel_name, user, content['file'])
+
+        text = content.get('text', '')
+
+        if AT_MATTBOT in text:
+            bot_command = text.split(AT_MATTBOT)[1].strip().lower()
+            return handle_command(channel, user, bot_command)
+
+        if channel in slack_ims and user != MATTBOT_ID:
+            return handle_command(channel, user, text)
+
+        if user != MATTBOT_ID:
+            print('{user} says: "{message}" in {channel}'.format(user=slack_users[user],
+                                                                 message=text,
+                                                                 channel=channel_name))
+
+
+class MattBot(object):
+    """
+    The Slack Bot
+
+    """
+    def __init__(self, config):
+        """
+
+        Args:
+            config:
+        """
+        self.name = config.BOT_NAME
+        self.read_websocket_delay = config.READ_WEBSOCKET_DELAY
+
+        self.token = os.environ.get(config.BOT_ENV_TOKEN)
+        print('Attempting to connect with token: {}'.format(self.token))
+        self.slack = SlackClient(self.token)
+
+        if not self.slack.rtm_connect():
+            print('Connection failed, invalid Slack token.')
+            sys.exit()
+
+        self.fetch_users()
+        self.fetch_channels()
+        self.fetch_ims()
+
+        self.bot_id = self.user_names_ids[self.name]
+        self.bot_mentioned = '<@{}>'.format(self.bot_id)
+
+        print('MattBot connected and running!')
+
+    def run(self):
+        """
+
+        Returns:
+
+        """
+        while True:
+            rtm_messages = self.slack.rtm_read()
+            for rtm_message in rtm_messages:
+                try:
+                    message = Message(self, rtm_message)
+                    self.handle_message(message)
+                except AttributeError as error:
+                    print(str(error))
+
+            time.sleep(self.read_websocket_delay)
+
+    def handle_message(self, message):
+        """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+
+        """
+        print('>> Handling message {} from channel {}'.format(message.content, channel))
+
+        command_type, _, parameters = bot_command.partition(' ')
+        command_class = '{}Command'.format(command_type.capitalize())
+
+        try:
+            handler_class = getattr(commands, command_class)
+            handler = handler_class(slack_client, slack_users, slack_channels, slack_ims)
+            return handler.run(channel, user, parameters)
+        except AttributeError:
+            response = 'Not sure what you mean.'
+            slack_client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
+
+    def fetch_users(self):
+        """
+
+        """
+        self.user_names_ids = {}
+        self.user_ids_name = {}
+
+        api_response = self.slack.api_call('users.list')
+        if api_response.get('ok', False):
+            users = api_response.get('members')
+            for user in users:
+                user_id = user.get('id')
+                user_name = user.get('name')
+                self.user_names_ids[user_name] = user_id
+                self.user_ids_name[user_id] = user_name
+
+    def fetch_channels(self):
+        """
+
+        """
+        self.channel_names_ids = {}
+        self.channel_ids_names = {}
+
+        api_response = self.slack.api_call('channels.list')
+        if api_response.get('ok', False):
+            channels = api_response.get('channels')
+            for channel in channels:
+                channel_id = channel.get('id')
+                channel_name = channel.get('name')
+                self.channel_names_ids[channel_name] = channel_id
+                self.channel_ids_names[channel_id] = channel_name
+
+    def fetch_ims(self):
+        """
+
+        """
+        self.im_names_ids = {}
+        self.im_ids_names = {}
+
+        api_response = self.slack.api_call('im.list')
+        if api_response.get('ok', False):
+            # retrieve all channels so we can find our bot
+            ims = api_response.get('ims')
+            for im in ims:
+                im_id = im.get('id')
+                im_user = im.get('user')
+                self.channel_names_ids[im_user] = im_id
+                self.channel_ids_names[im_id] = im_user
 
 
 def check_deploy_errors(lines):
@@ -175,154 +304,7 @@ def handle_file(channel, user, file_data):
     slack_client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
 
 
-def parse_slack_output(slack_rtm_output):
-    """
-    The Slack Real Time Messaging API is an events firehose.
-    this parsing function returns None unless a message is
-    directed at the Bot, based on its ID.
-
-    """
-    if not slack_rtm_output:
-        return None, None
-
-    # print('Parsing slack message: {}'.format(slack_rtm_output))
-
-    for output in slack_rtm_output:
-
-        message_type = output.get('type', None)
-        if message_type != 'message':
-            # print('\t-- skipping type {}'.format(message_type))
-            continue
-
-        user = output.get('user')
-
-        channel = output.get('channel')
-        try:
-            channel_name = slack_channels[channel]
-        except KeyError:
-            if channel in slack_ims:
-                channel_name = 'private messages'
-
-        if 'file' in output:
-            return handle_file(channel_name, user, output['file'])
-
-        text = output.get('text', '')
-
-        if AT_MATTBOT in text:
-            bot_command = text.split(AT_MATTBOT)[1].strip().lower()
-            return handle_command(channel, user, bot_command)
-
-        if channel in slack_ims and user != MATTBOT_ID:
-            return handle_command(channel, user, text)
-
-        if user != MATTBOT_ID:
-            print('{user} says: "{message}" in {channel}'.format(user=slack_users[user],
-                                                                 message=text,
-                                                                 channel=channel_name))
-
-
-def get_users():
-    """
-
-    :return:
-    """
-    slack_users = {}
-
-    api_call = slack_client.api_call('users.list')
-    if api_call.get('ok', False):
-        # retrieve all users so we can find our bot
-        users = api_call.get('members')
-        for user in users:
-            user_id = user.get('id')
-            user_name = user.get('name', None)
-            slack_users[user_id] = user_name
-
-    return slack_users
-
-
-def get_channels():
-    """
-
-    :return:
-    """
-    slack_channels = {}
-
-    api_call = slack_client.api_call('channels.list')
-    if api_call.get('ok', False):
-        # retrieve all channels so we can find our bot
-        channels = api_call.get('channels')
-        for channel in channels:
-            channel_id = channel.get('id')
-            channel_name = channel.get('name', None)
-            slack_channels[channel_id] = channel_name
-
-    return slack_channels
-
-
-def get_ims():
-    """
-
-    :return:
-    """
-    slack_ims = {}
-
-    api_call = slack_client.api_call('im.list')
-    if api_call.get('ok', False):
-        # retrieve all channels so we can find our bot
-        ims = api_call.get('ims')
-        for im in ims:
-            im_id = im.get('id')
-            im_user = im.get('user', None)
-            slack_ims[im_id] = im_user
-
-    return slack_ims
-
-
-def get_user_id(users, name):
-    """
-    Get the user id for th given username
-
-    Args:
-        users: dict
-        name: string
-
-    Returns:
-        integer
-
-    """
-    for user_id, user_name in users.items():
-        if user_name == name:
-            return user_id
-
-    return None
-
 
 if __name__ == "__main__":
-
-    MATTBOT_TOKEN = os.environ.get('MATTBOT_TOKEN')
-    print('Attempting to connect with token: {}'.format(MATTBOT_TOKEN))
-    slack_client = SlackClient(MATTBOT_TOKEN)
-
-    if not slack_client.rtm_connect():
-        print("Connection failed, invalid Slack token.")
-        sys.exit()
-
-    print('MattBot connected and running!')
-
-    slack_users = get_users()
-    print ('\n'.join([k+':'+v for k, v in slack_users.items()]))
-    MATTBOT_ID = get_user_id(slack_users, MATTBOT_NAME)
-    print('Bot id: {}'.format(MATTBOT_ID))
-    AT_MATTBOT = '<@{}>'.format(MATTBOT_ID)
-
-    slack_channels = get_channels()
-    print('\nChannels')
-    print ('\n'.join([k + ':' + v for k, v in slack_channels.items()]))
-
-    slack_ims = get_ims()
-    print ('\nIMs')
-    print ('\n'.join([k + ':' + v for k, v in slack_ims.items()]))
-
-    while True:
-        parse_slack_output(slack_client.rtm_read())
-        time.sleep(READ_WEBSOCKET_DELAY)
+    bot = MattBot(config.Configuration)
+    bot.run()
